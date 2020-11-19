176145c267ea1811d3be6b631367022502ecb2be
Patima Somlert <patima.puii@gmail.com>
2020-11-19 21:44:11 +0700

    Create Method GetUserList into Controller

--- HospitalAPI/Controllers/UserController.cs
+++ HospitalAPI/Controllers/UserController.cs
@@ -7,7 +7,8 @@ namespace HospitalAPI.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-    public class UserController : ControllerBase {
+    public class UserController : ControllerBase
+    {
         private readonly IUserService _userService;
 
         public UserController(IUserService userService)
@@ -17,10 +18,18 @@ namespace HospitalAPI.Controllers
 
         [HttpPost]
         [Route("login")]
-        public ResposeModel Login([FromBody] RequestLogin item) {
-           ResposeModel result =  _userService.ValidateLogin(item);
-           return result;
+        public ResposeModel Login([FromBody] RequestLogin item)
+        {
+            ResposeModel result = _userService.ValidateLogin(item);
+            return result;
         }
 
+        [HttpGet]
+        [Route("UserInfo")]
+        public UserModelList GetUserList()
+        {
+            UserModelList result = _userService.SelectUsers();
+            return result;
+        }
     }
 }
