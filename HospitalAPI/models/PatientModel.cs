03f513fdd18ecf5568a84b90b99c8bddae1b617c
Patima Somlert <patima.puii@gmail.com>
2020-11-25 10:46:21 +0700

    decorate format document

--- HospitalAPI/models/PatientModel.cs
+++ HospitalAPI/models/PatientModel.cs
@@ -1,32 +1,35 @@
 using System;
 using System.Collections.Generic;
 
-namespace HospitalAPI 
+namespace HospitalAPI
 {
     public class PatientModel
     {
-        public int hn {get; set;}
-        public string name {get; set;}
-        public string surname {get; set;}
-        public int age {get; set;}
-        public DateTime birthday {get; set;}
-        public int typeId {get; set;}
-        public string typeName {get; set;}
-        public int visit {get; set;}
-        public DateTime appointment {get; set;}
-        public string doctor {get; set;}
+        public int hn { get; set; }
+        public string name { get; set; }
+        public string surname { get; set; }
+        public int age { get; set; }
+        public DateTime birthday { get; set; }
+        public int typeId { get; set; }
+        public string typeName { get; set; }
+        public int visit { get; set; }
+        public DateTime appointment { get; set; }
+        public string doctor { get; set; }
     }
 
-    public class PatientModelList {
-        public List<PatientModel> Patienttable {get; set;}
+    public class PatientModelList
+    {
+        public List<PatientModel> Patienttable { get; set; }
     }
 
-    public class PatientModelRequest {
+    public class PatientModelRequest
+    {
         public string SearchText { get; set; }
         public int TypeId { get; set; }
     }
 
-        public class PatientRequestIdModel {
+    public class PatientRequestIdModel
+    {
         public int Id { get; set; }
     }
 
